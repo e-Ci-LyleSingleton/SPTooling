2b2c6b1af6b4193c5130beddc07ea2b1d37f8927
e-Ci-LyleSingleton <lsingleton@e-ci.com.au>
2017-08-31 11:26:35 +1000

    added basic document migrator

---
+++ SPTooling/AddListItemWitId/AddListItemWithId.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.SharePoint;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Coliban.SharePoint.Tooling
+{
+    class AddListItemWithId
+    {
+        protected static int itemId;
+
+        protected static string siteUrl;
+        protected static string webRelativeUrl;
+        protected static string listName;
+        protected static string propMappingFile;
+        
+        protected static SPSite site;
+        protected static SPWeb web;
+        protected static SPList list;
+
+
+        static void Main(string[] args)
+        {
+            itemId = 0;
+            siteUrl = string.Empty;
+            webRelativeUrl = string.Empty;
+            listName = string.Empty;
+            propMappingFile = string.Empty;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string argumentString = args[i];
+
+                string argumentValue = string.Empty;
+
+                if (i + 1 < args.Length)
+                {
+                    argumentValue = args[i + 1].Trim();
+                }
+
+                switch (argumentString)
+                {
+                    case "-itemId":
+                        {
+                            int.TryParse(argumentValue, out itemId);
+                            i++;
+                            break;
+                        }
+                    case "-siteUrl":
+                        {
+                            siteUrl = argumentValue;
+                            i++;
+                            break;
+                        }
+                    case "-webUrl":
+                        {
+                            webRelativeUrl = argumentValue;
+                            i++;
+                            break;
+                        }
+                    case "-listName":
+                        {
+                            listName = argumentValue;
+                            i++;
+                            break;
+                        }
+                    case "-propMapping":
+                        {
+                            propMappingFile = argumentValue;
+                            i++;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Unknown argument: " + argumentString);
+                        return;
+                }
+            }
+
+
+            if (itemId == 0)
+            {
+                Console.WriteLine("you must specify a valid item Id with -itemId <Id>");
+                return;
+            }
+            else if (siteUrl == string.Empty)
+            {
+                Console.WriteLine("you must specify a site collection url with -siteUrl <SiteUrl>");
+                return;
+            }
+            else if (webRelativeUrl == string.Empty)
+            {
+                Console.WriteLine("you must specify a site collection relative site url with -webUrl <WebUrl>");
+                return;
+            }
+            else if (listName == string.Empty)
+            {
+                Console.WriteLine("you must specify a SharePoint list name with -listName <ListName>");
+                return;
+            }
+            else if (propMappingFile == string.Empty)
+            {
+                Console.WriteLine("you must specify a property mapping file with -propMapping <FilePath>");
+                return;
+            }
+
+            try
+            {
+
+                Console.WriteLine("Connecting to site collection at '" + siteUrl + "'");
+                site = new SPSite(siteUrl);
+
+                web = site.OpenWeb(webRelativeUrl);
+                Console.WriteLine("Opened site '" + web.Title + "'");
+
+                list = web.Lists[listName];
+                Console.WriteLine("Opened list '" + list.Title + "'");
+                
+                SPQuery existingItemQuery = new SPQuery();
+                existingItemQuery.ViewXml = @"
+<View>
+    <Query>
+        <Where>
+            <Eq>
+                <FieldRef Name='ID' />
+                <Value Type='Integer'>" + itemId.ToString() + @"</Value>
+            </Eq>
+        </Where>
+    </Query>
+    <ViewFields>
+      <FieldRef Name='Title'/>
+    </ViewFields>
+</View>";
+
+                SPListItemCollection existingItems = list.GetItems(existingItemQuery);
+                if( existingItems.Count != 0 )
+                {
+                    throw new ArgumentOutOfRangeException("Item id '" + existingItems[0][SPBuiltInFieldId.Title]+"' is already in use.");
+                }
+
+                // The ID field is readonly field, set to to read and write mode.
+                list.Fields[SPBuiltInFieldId.ID].ReadOnlyField = false;
+                list.Update();
+
+                JsonSerializerSettings settings = new JsonSerializerSettings { Converters = new JsonConverter[] { new Common.JsonGenericDictionaryOrArrayConverter() } };
+
+                Dictionary<string, string> propMaps = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(propMappingFile), settings);
+
+                SPListItem item = list.AddItem();
+
+                foreach (string key in propMaps.Keys)
+                {
+                    item[key] = propMaps[key];
+                }
+
+                item[SPBuiltInFieldId.ID] = itemId;
+                item.Update();
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if( list != null )
+                {
+                    if (list.Fields[SPBuiltInFieldId.ID].ReadOnlyField != true)
+                    {
+                        list.Fields[SPBuiltInFieldId.ID].ReadOnlyField = true;
+                        list.Update();
+                    }
+                }
+                if( site != null )
+                {
+                    site.Dispose();
+                }
+                if( web != null )
+                {
+                    web.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ SPTooling/DocumentMigrator/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext sourceCtx = new ClientContext(Properties.Resources.sourceWebUrl);
+            ClientContext destCtx = new ClientContext(Properties.Resources.destWebUrl);
+
+            List sourceList = sourceCtx.Web.Lists.GetByTitle(Properties.Resources.sourceDocLibName);
+            List destList = destCtx.Web.Lists.GetByTitle(Properties.Resources.destDocLibName);
+
+            View migratingItemsView = sourceList.Views.GetById(new Guid(Properties.Resources.sourceViewId));
+            
+            sourceCtx.Load(migratingItemsView);
+            sourceCtx.ExecuteQuery();
+
+            CamlQuery migratingItemsCaml = new CamlQuery();
+            migratingItemsCaml.ViewXml = migratingItemsView.HtmlSchemaXml;
+
+            ListItemCollection migratingItemsList = sourceList.GetItems(migratingItemsCaml);
+
+            sourceCtx.Load(migratingItemsList);
+            sourceCtx.ExecuteQuery();
+
+            IEnumerator<ListItem> itemIter = migratingItemsList.GetEnumerator();
+
+            while(itemIter.MoveNext())
+            {
+                ListItem itemToMigrate = itemIter.Current;
+                if ( itemToMigrate.FileSystemObjectType == FileSystemObjectType.Folder )
+                {
+                    string libRelFolderUrl = GetNormalisedFileRef(itemToMigrate);
+                    string folderName = itemToMigrate.FieldValues["FileLeafRef"] as string;
+                    DateTime modifiedDate = (DateTime)itemToMigrate.FieldValues["Modified"];
+                    Console.WriteLine(modifiedDate + " Folder: " + libRelFolderUrl);
+                    // Check if folder exists
+                }
+                else if( itemToMigrate.FileSystemObjectType == FileSystemObjectType.File )
+                {
+                    string libRelFolderUrl = GetNormalisedFileRef(itemToMigrate);
+                    string fileName = itemToMigrate.FieldValues["FileLeafRef"] as string;
+                    int fileNameIndex = libRelFolderUrl.LastIndexOf(fileName);
+                    string sourceFolder = libRelFolderUrl.Remove(fileNameIndex-1, fileName.Length+1);
+                    string author = (itemToMigrate.FieldValues["Author"] as FieldUserValue).LookupValue;
+                    string modifier = (itemToMigrate.FieldValues["Editor"] as FieldUserValue).LookupValue;
+                    DateTime modified = (DateTime)itemToMigrate.FieldValues["Modified"];
+                    DateTime created = (DateTime)itemToMigrate.FieldValues["Created"];
+
+                    Console.WriteLine(modified + " \"" + fileName + "\" in \"" + sourceFolder + "\"");
+
+                    // check if file already exists
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static string GetNormalisedFileRef(ListItem item)
+        {
+            string result = item.FieldValues["FileRef"] as string;
+
+            if( !item.ParentList.RootFolder.IsObjectPropertyInstantiated("ServerRelativeUrl") )
+            {
+                item.Context.Load(item.ParentList.RootFolder);
+                item.Context.ExecuteQuery();
+            }
+
+            string libraryRootUrl = item.ParentList.RootFolder.ServerRelativeUrl;
+            int libraryRootUrlIndex = result.IndexOf(libraryRootUrl);
+
+            if (libraryRootUrlIndex == 0 )
+            {
+                result = result.Substring(libraryRootUrl.Length, result.Length - libraryRootUrl.Length);
+            }
+            else
+            {
+                throw new ArgumentException("fileRef from library", "list");
+            }
+            return result;
+        }
+    }
+}
